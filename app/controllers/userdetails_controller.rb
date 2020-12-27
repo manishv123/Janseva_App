483eca25f91d4ec61af4fb852a6d4fb93bec8460
manishv123 <mv005123@gmail.com>
2020-12-28 02:17:40 +0530

    Added edit

--- app/controllers/userdetails_controller.rb
+++ app/controllers/userdetails_controller.rb
@@ -1,5 +1,6 @@
 class UserdetailsController < ApplicationController
     before_action :require_user
+    before_action :set_user,only: [:edit,:update,:show]
     def new
         @userdetails=Userdetail.new
     end
@@ -7,14 +8,33 @@ class UserdetailsController < ApplicationController
     @userdetail=Userdetail.new(userdetail_params)
     @userdetail.uuid=current_user.id;
             if @userdetail.save
-                flash[:success]="Successfully Applied"
+                flash[:success]="Successfully Added Details"
                 redirect_to root_path
             else
                 render :new
             end
     end
+
+    def edit
+       
+    end
+    def show
+
+    end
+    def update
+        if @userdetail.update(userdetail_params)
+            flash[:success]="Successfully Updated" 
+            redirect_to root_path
+        else
+            render 'edit'
+        end
+    end
+
     private
     def userdetail_params
         params.require(:userdetail).permit(:name, :gender, :age, :address, :pincode, :phone, :aadhaar)
+    end
+    def set_user
+        @userdetails=Userdetail.find(params[:id])
     end
 end
