86c11a733272a633edc19a789261387e589fa15f
manishv123 <mv005123@gmail.com>
2020-12-27 17:21:44 +0530

    Added table in admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_082851) do
+ActiveRecord::Schema.define(version: 2020_12_26_084633) do
 
   create_table "openings", force: :cascade do |t|
     t.string "title"
